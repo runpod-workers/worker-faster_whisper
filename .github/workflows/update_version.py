8ebba05ce5dfac278311adc6fdaddffea42dccc8
Justin Merrell <merrelljustin@gmail.com>
2023-06-14 12:04:03 -0400

    Update update_version.py

--- .github/workflows/update_version.py
+++ .github/workflows/update_version.py
@@ -2,7 +2,11 @@
 Checks for an update of the runpod package
 """
 
-new_version = "0.0.1"
+import requests
+
+# Get current version
+new_version = requests.get("https://pypi.org/pypi/runpod/json",
+                           timeout=10).json()["info"]["version"]
 
 # Update requirements.txt
 with open("./builder/requirements.txt", "r", encoding="UTF-8") as req_file:
