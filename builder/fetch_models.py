117e64977f94038f988be687cc97dfd8f468a441
Gabe <gabewillen@gmail.com>
2025-02-07 14:02:05 -0600

    added missing os import

--- builder/fetch_models.py
+++ builder/fetch_models.py
@@ -2,6 +2,7 @@ from concurrent.futures import ThreadPoolExecutor
 from faster_whisper import WhisperModel
 from runpod import RunPodLogger
 from urllib.parse import urlparse
+import os
 
 logger = RunPodLogger()
 model_names = [
