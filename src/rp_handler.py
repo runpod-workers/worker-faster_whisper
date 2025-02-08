48523679609205873008bf9a025f29c87d5744f9
efron <efron.dev@gmail.com>
2025-02-07 20:24:55 -0800

    checkpoint: rp_handler

--- src/rp_handler.py
+++ src/rp_handler.py
@@ -1,101 +1,37 @@
-"""
-rp_handler.py for runpod worker
-
-rp_debugger:
-- Utility that provides additional debugging information.
-The handler must be called with --rp_debugger flag to enable it.
-"""
+from typing import Any
 import base64
-import tempfile
-
-from rp_schema import INPUT_VALIDATIONS
-from runpod.serverless.utils import download_files_from_urls, rp_cleanup, rp_debugger
-from runpod.serverless.utils.rp_validator import validate
-import runpod
 import predict
+import requests
+import runpod
+import tempfile
+def run(model: predict.Predictor, job: dict[str, Any]) -> dict[str, Any]:
+    input: dict[str, Any] = (job['input'])
+    id: str = job['id']
 
+    # open a temporary file to write the audio content
+    # either we'll download the audio from a URL ("audio" key)
+    # or just dump base64 content to the file ("audio_base64" key)
+    with tempfile.NamedTemporaryFile(suffix=id) as tmp:
+        if "audio" in input and "audio_base64" in input: # both keys present: don't guess
+            raise ValueError("bad request: expected keys 'audio' or 'audio_base64', not both")
+        if (audiopath := input.get('audio', None)) is not None and (resp := requests.get(audiopath, stream=True)).status_code == 200: # URL
+            for chunk in resp.iter_content(1024*32): # 32KiB chunks
+                tmp.write(chunk)   
+        elif (audiopath := input.get('audio_base64', None)) is not None: # base64 encoded audio
+            tmp.write(base64.b64decode(audiopath))
+        else:
+            raise ValueError("bad request: expected keys 'audio' or 'audio_base64', not both")
+        tmp.seek(0)
+        # pass the rest of the input in as kwargs; remove the audio and model keys
+        kwargs = {k: v for k, v in input.items() if k not in {"audio", "audio_base64", "model", "model_name"}} 
 
-MODEL = predict.Predictor()
-MODEL.setup()
-
-
-def base64_to_tempfile(base64_file: str) -> str:
-    '''
-    Convert base64 file to tempfile.
-
-    Parameters:
-    base64_file (str): Base64 file
-
-    Returns:
-    str: Path to tempfile
-    '''
-    with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as temp_file:
-        temp_file.write(base64.b64decode(base64_file))
-
-    return temp_file.name
-
-
-@rp_debugger.FunctionTimer
-def run_whisper_job(job):
-    '''
-    Run inference on the model.
-
-    Parameters:
-    job (dict): Input job containing the model parameters
-
-    Returns:
-    dict: The result of the prediction
-    '''
-    job_input = job['input']
-
-    with rp_debugger.LineTimer('validation_step'):
-        input_validation = validate(job_input, INPUT_VALIDATIONS)
-
-        if 'errors' in input_validation:
-            return {"error": input_validation['errors']}
-        job_input = input_validation['validated_input']
-
-    if not job_input.get('audio', False) and not job_input.get('audio_base64', False):
-        return {'error': 'Must provide either audio or audio_base64'}
-
-    if job_input.get('audio', False) and job_input.get('audio_base64', False):
-        return {'error': 'Must provide either audio or audio_base64, not both'}
-
-    if job_input.get('audio', False):
-        with rp_debugger.LineTimer('download_step'):
-            audio_input = download_files_from_urls(job['id'], [job_input['audio']])[0]
-
-    if job_input.get('audio_base64', False):
-        audio_input = base64_to_tempfile(job_input['audio_base64'])
-
-    with rp_debugger.LineTimer('prediction_step'):
-        whisper_results = MODEL.predict(
-            audio=audio_input,
-            model_name=job_input["model"],
-            transcription=job_input["transcription"],
-            translation=job_input["translation"],
-            translate=job_input["translate"],
-            language=job_input["language"],
-            temperature=job_input["temperature"],
-            best_of=job_input["best_of"],
-            beam_size=job_input["beam_size"],
-            patience=job_input["patience"],
-            length_penalty=job_input["length_penalty"],
-            suppress_tokens=job_input.get("suppress_tokens", "-1"),
-            initial_prompt=job_input["initial_prompt"],
-            condition_on_previous_text=job_input["condition_on_previous_text"],
-            temperature_increment_on_fallback=job_input["temperature_increment_on_fallback"],
-            compression_ratio_threshold=job_input["compression_ratio_threshold"],
-            logprob_threshold=job_input["logprob_threshold"],
-            no_speech_threshold=job_input["no_speech_threshold"],
-            enable_vad=job_input["enable_vad"],
-            word_timestamps=job_input["word_timestamps"]
+        return model.predict(
+            audio=tmp.name,
+            model_name=input.get("model", input.get("model_name", "base")),
+            **kwargs
         )
 
-    with rp_debugger.LineTimer('cleanup_step'):
-        rp_cleanup.clean(['input_objects'])
-
-    return whisper_results
-
-
-runpod.serverless.start({"handler": run_whisper_job})
+if __name__ == "__main__":
+    MODEL = predict.Predictor()
+    MODEL.setup()
+    runpod.serverless.start({"handler": lambda job: run(MODEL, job)})
